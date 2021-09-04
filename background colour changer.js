9993993041270c0ef23c2506d894a897c3ec349d
rlodha2610 <88890192+rlodha2610@users.noreply.github.com>
2021-09-04 15:33:52 +0530

    Add files via upload

---
+++ background colour changer.js
@@ -0,0 +1,42 @@
+var square;
+var edges;
+function setup() {
+  createCanvas(400,400);
+  square=createSprite(200,200,40,40);
+
+  edges=createEdgeSprites();
+}
+
+function draw() 
+{
+  background('yellow');
+
+  
+  
+  
+  if (keyDown("right")){
+    square.x = square.x+2;
+  }
+  if (keyDown("left")){
+    square.x = square.x-2;
+  }
+  if (keyDown("up")){
+      square.y = square.y-2;
+  }
+
+  if (keyDown("down")){
+    square.y = square.y+2;
+}
+   if(keyIsDown(DOWN_ARROW))
+      {
+        background('red');
+      }
+
+  square.bounceOff(edges);
+  
+  drawSprites();
+}
+
+
+
+
